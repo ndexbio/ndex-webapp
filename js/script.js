7caa7700df894ce7f44fd0236b9e16bf85e19104
Dexter Pratt <dexterpratt@Dexters-iMac.local>
2014-12-12 10:05:51 -0800

    Hopefully last tweaks :-) for 1.1

--- js/script.js
+++ js/script.js
@@ -260,10 +260,13 @@ ndexApp.controller('mainController', ['ndexService', 'ndexUtility', 'sharedPrope
         $scope.signIn.newUser = {};
 
         $scope.signIn.submitSignIn = function () {
-            ndexService.signIn($scope.signIn.accountName, $scope.signIn.password).success(function (userData) {
-                sharedProperties.setCurrentUser(userData.externalId, userData.accountName); //this info will have to be sent via emit if we want dynamic info on the nav bar
-                $scope.$emit('LOGGED_IN'); //Angular service capability, shoot a signal up the scope tree notifying parent scopes this event occurred, see mainController
-                $location.path("/user/" + userData.externalId);
+            ndexService.signIn($scope.signIn.accountName, $scope.signIn.password)
+                .success(function (userData) {
+                    sharedProperties.setCurrentUser(userData.externalId, userData.accountName); //this info will have to be sent via emit if we want dynamic info on the nav bar
+                    $scope.$emit('LOGGED_IN'); //Angular service capability, shoot a signal up the scope tree notifying parent scopes this event occurred, see mainController
+                    $location.path("/user/" + userData.externalId);
+                    $scope.signIn.accountName = null;
+                    $scope.signIn.password = null;
             }).error(function (error) {
                 $scope.signIn.message = error;
             });
