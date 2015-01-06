aa5999cd1db10aa8963531f16edf6157d46e0051
David Welker <david.v.welker@gmail.com>
2015-01-06 11:07:37 -0800

    Making upload more robust.

--- js/controllers/upload-controller.js
+++ js/controllers/upload-controller.js
@@ -9,6 +9,20 @@ ndexApp.controller('uploadController',
             $scope.uploadController = {};
             var uploadController = $scope.uploadController;
             uploadController.fileSizeError = false;
+            uploadController.fileExtensionError = false;
+
+            $scope.removeItemFromQueue = function (item){
+                uploadController.fileExtensionError = false;
+                uploadController.fileSizeError = false;
+                item.remove();
+            }
+
+            $scope.removeAllFromQueue = function(uploader){
+                uploadController.fileExtensionError = false;
+                uploadController.fileSizeError = false;
+                uploader.clearQueue();
+            }
+
 
 
             $scope.refreshTasks = function(){
@@ -59,6 +73,13 @@ ndexApp.controller('uploadController',
                 }
             });
 
+            //uploader.autoUpload = true;
+            uploader.removeAfterUpload = true;
+
+
+            //Queue size is the amount of data in the queue.
+            uploader.queueSize = 0;
+
             // FILTERS
             // item is {File|FileLikeObject}
             uploader.filters.push({
@@ -69,26 +90,50 @@ ndexApp.controller('uploadController',
             });
 
             uploader.filters.push({
-                name: 'fileSizeFilter',
-                fn: function(item, options) {
-                    return item.size < 10000000;
+                name: 'fileExtensionFilter',
+                fn: function(item, options){
+                    var allowed = ['xgmml', 'xbel', 'sif'];
+                    var ext = item.name.split(".").pop().toLowerCase();
+                    return $.inArray(ext, allowed) != -1;
                 }
             });
+
+            uploader.filters.push({
+                name: 'fileSizeFilter',
+                fn: function(item, options)
+                {
+                    this.queueSize = 0;
+                    for( var i = 0; i < this.queue.length; i++ )
+                    {
+                        this.queueSize += this.queue[i].file.size;
+                    }
+                    var maxSize = this.queueSize + item.size;
+                    return maxSize < 10000000;
+                }
+            });
+
+
 
 
             // CALLBACKS
 
             // /*{File|FileLikeObject}*/
             uploader.onWhenAddingFileFailed = function(item, filter, options) {
+                if( filter.name === 'fileExtensionFilter' )
+                    uploadController.fileExtensionError = true;
                 if( filter.name === 'fileSizeFilter' )
                     uploadController.fileSizeError = true;
+
                 //console.info('onWhenAddingFileFailed', item, filter, options);
             };
             uploader.onAfterAddingFile = function(fileItem) {
+                uploadController.fileExtensionError = false;
                 uploadController.fileSizeError = false;
                 //console.info('onAfterAddingFile', fileItem);
             };
             uploader.onAfterAddingAll = function(addedFileItems) {
+                uploadController.fileExtensionError = false;
+                uploadController.fileSizeError = false;
                 //console.info('onAfterAddingAll', addedFileItems);
             };
             uploader.onBeforeUploadItem = function(item) {
@@ -103,19 +148,27 @@ ndexApp.controller('uploadController',
                 //console.info('onProgressAll', progress);
             };
             uploader.onSuccessItem = function(fileItem, response, status, headers) {
+                uploadController.fileExtensionError = false;
+                uploadController.fileSizeError = false;
                 //console.info('onSuccessItem', fileItem, response, status, headers);
             };
             uploader.onErrorItem = function(fileItem, response, status, headers) {
                 //console.info('onErrorItem', fileItem, response, status, headers);
             };
             uploader.onCancelItem = function(fileItem, response, status, headers) {
+                uploadController.fileExtensionError = false;
+                uploadController.fileSizeError = false;
                 //console.info('onCancelItem', fileItem, response, status, headers);
             };
             uploader.onCompleteItem = function(fileItem, response, status, headers) {
+                uploadController.fileExtensionError = false;
+                uploadController.fileSizeError = false;
                 //console.log('onCompleteItem', fileItem, response, status, headers);
                 //console.info('onCompleteItem', fileItem, response, status, headers);
             };
             uploader.onCompleteAll = function() {
+                uploadController.fileExtensionError = false;
+                uploadController.fileSizeError = false;
                 //console.info('onCompleteAll');
                 //console.log('onCompleteAll');
                 $scope.refreshTasks();
