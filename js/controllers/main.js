705f9846780245c5ff24a1d6bbde6ba1388b8dbb
vrynkov <vrynkov@yahoo.com>
2016-05-19 15:13:03 -0700

    In index.html commented out code that shows Home menu; in main.js, fixed some typos and added section that handles config.searchDocLink.

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -499,7 +499,7 @@ ndexApp.controller('mainController', ['config', 'ndexService', 'ndexUtility', 's
                 config.logoLink = {};
             }
             if (typeof config.logoLink.href === 'undefined') {
-                config.logoLink.href = "http://www.ndexbio.org";
+                config.logoLink.href = "http://public.ndexbio.org";
             }
             if (typeof config.logoLink.warning === 'undefined') {
                 config.logoLink.warning = "Warning! You are about to leave your organization's domain. Follow this link?";
@@ -507,6 +507,25 @@ ndexApp.controller('mainController', ['config', 'ndexService', 'ndexUtility', 's
             if (typeof config.logoLink.showWarning === 'undefined') {
                 config.logoLink.showWarning = false;
             }
+
+
+/*
+            if (typeof config.homeLink === 'undefined') {
+                config.homeLink = {};
+            }
+            if (typeof config.homeLink.label === 'undefined') {
+                config.homeLink.label = "Home";
+            }
+            if (typeof config.homeLink.href === 'undefined') {
+                config.homeLink.href = "http://www.ndexbio.org/home";
+            }
+            if (typeof config.homeLink.warning === 'undefined') {
+                config.homeLink.warning = "Warning! You are about to leave your organization's domain. Follow this link?";
+            }
+            if (typeof config.homeLink.showWarning === 'undefined') {
+                config.homeLink.showWarning = false;
+            }
+*/
 
             if (typeof config.aboutLink === 'undefined') {
                 config.aboutLink = {};
@@ -518,7 +537,7 @@ ndexApp.controller('mainController', ['config', 'ndexService', 'ndexUtility', 's
                 config.aboutLink.href = "http://www.ndexbio.org/about-ndex";
             }
             if (typeof config.aboutLink.warning === 'undefined') {
-                config.logoLink.warning = "Warning! You are about to leave your organization's domain. Follow this link?";
+                config.aboutLink.warning = "Warning! You are about to leave your organization's domain. Follow this link?";
             }
             if (typeof config.aboutLink.showWarning === 'undefined') {
                 config.aboutLink.showWarning = false;
@@ -534,7 +553,7 @@ ndexApp.controller('mainController', ['config', 'ndexService', 'ndexUtility', 's
                 config.documentationLink.href = "http://www.ndexbio.org/quick-start";
             }
             if (typeof config.documentationLink.warning === 'undefined') {
-                config.logoLink.warning = "Warning! You are about to leave your organization's domain. Follow this link?";
+                config.documentationLink.warning = "Warning! You are about to leave your organization's domain. Follow this link?";
             }
             if (typeof config.documentationLink.showWarning === 'undefined') {
                 config.documentationLink.showWarning = false;
@@ -551,7 +570,7 @@ ndexApp.controller('mainController', ['config', 'ndexService', 'ndexUtility', 's
                 config.apiLink.href = "#/api";
             }
             if (typeof config.apiLink.warning === 'undefined') {
-                config.logoLink.warning = "Warning! You are about to leave your organization's domain. Follow this link?";
+                config.apiLink.warning = "Warning! You are about to leave your organization's domain. Follow this link?";
             }
             if (typeof config.apiLink.warn === 'undefined') {
                 config.apiLink.warn = false;
@@ -567,7 +586,7 @@ ndexApp.controller('mainController', ['config', 'ndexService', 'ndexUtility', 's
                 config.reportBugLink.href = "http://www.ndexbio.org/report-a-bug";
             }
             if (typeof config.reportBugLink.warning === 'undefined') {
-                config.logoLink.warning = "Warning! You are about to leave your organization's domain. Follow this link?";
+                config.reportBugLink.warning = "Warning! You are about to leave your organization's domain. Follow this link?";
             }
             if (typeof config.reportBugLink.showWarning === 'undefined') {
                 config.reportBugLink.showWarning = false;
@@ -583,11 +602,29 @@ ndexApp.controller('mainController', ['config', 'ndexService', 'ndexUtility', 's
                 config.contactUsLink.href = "http://www.ndexbio.org/contact-us/";
             }
             if (typeof config.contactUsLink.warning === 'undefined') {
-                config.logoLink.warning = "Warning! You are about to leave your organization's domain. Follow this link?";
+                config.contactUsLink.warning = "Warning! You are about to leave your organization's domain. Follow this link?";
             }
             if (typeof config.contactUsLink.showWarning === 'undefined') {
                 config.contactUsLink.showWarning = false;
             }
+
+
+            if (typeof config.searchDocLink === 'undefined') {
+                config.searchDocLink = {};
+            }
+            if (typeof config.searchDocLink.label === 'undefined') {
+                config.searchDocLink.label = "Documentation on Searching in NDEx";
+            }
+            if (typeof config.searchDocLink.href === 'undefined') {
+                config.searchDocLink.href = "http://www.ndexbio.org/finding-and-querying-networks/";
+            }
+            if (typeof config.searchDocLink.warning === 'undefined') {
+                config.searchDocLink.warning = "Warning! You are about to leave your organization's domain. Follow this link?";
+            }
+            if (typeof config.searchDocLink.showWarning === 'undefined') {
+                config.searchDocLink.showWarning = false;
+            }
+
         }
 
 
