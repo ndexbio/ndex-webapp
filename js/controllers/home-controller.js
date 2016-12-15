4154347f4bb2a9a9974f7beca418384f4d4a39bc
vrynkov <vrynkov@yahoo.com>
2016-12-15 14:02:48 -0800

    Added featuredCollections to ndex-webapp-config.js file. Modified code to parse featuredCollections and show them on NDEx landing page. The links of featured collection use the server specified in ndexServerUriV2 in ndex-webapp-config.js

--- js/controllers/home-controller.js
+++ js/controllers/home-controller.js
@@ -1,9 +1,23 @@
-ndexApp.controller('homeController',
-    ['sharedProperties', '$location',
-        function (sharedProperties, $location) {
+ndexApp.controller('homeController', ['sharedProperties', 'config', '$scope',
+    function (sharedProperties, config, $scope) {
 
-            if( sharedProperties.getCurrentUserId() )
-                $location.path("/user/" + sharedProperties.getCurrentUserId() );
+        $scope.homeController = {};
+        var homeController = $scope.homeController;
+        homeController.featuredCollections = [];
+        homeController.URL = null;
 
+        if (config.featuredCollections) {
+            homeController.featuredCollections = config.featuredCollections;
+
+            var parser = document.createElement('a');
+            parser.href = config.ndexServerUriV2;
+
+            homeController.URL = parser.protocol + "//" + parser.hostname + "/#/";
         }
-    ]);
+
+        homeController.getURLOfFeaturedCollection = function(collection) {
+            return homeController.URL + collection.account + "/" + collection.UUID;
+        }
+        
+    }
+]);
