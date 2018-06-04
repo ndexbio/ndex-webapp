17c5a340e40e1497a7a952bdffbde3bc2fc12f56
vrynkov <vrynkov@yahoo.com>
2018-06-04 15:35:20 -0700

    Added functionality for keeping settings of My Networks (sorting column/order, page number and filters) table on MyAccount page throughout the session.

---
+++ app/scripts/services/user-session-tables-settings.js
@@ -0,0 +1,120 @@
+/**
+ * Created by vrynkov 4 June 2018
+ *
+ * This service is used to keep settings (Page Number, Sorting Column and COlumn Filters) of the
+ * My Networks table defined on MyAccount page throughout the session.
+ *
+ */
+'use strict';
+
+angular.module('ndexServiceApp')
+    .service('userSessionTablesSettings', ['$location','sharedProperties',
+        function ($location, sharedProperties) {
+
+            var self = this;
+
+            self.getCurrentUserName = function() {
+                return sharedProperties.getCurrentUserAccountName();
+            };
+
+            self.initMyAccountNetworkTableFiltersAndSorting = function() {
+
+                var userName = self.getCurrentUserName();
+
+                var myAccountNetworkTableFiltersAndSorting =
+                    (typeof(Storage) !== 'undefined') ? JSON.parse(sessionStorage.getItem(userName)) : null;
+
+                var lastReloadViewName = self.getCurrentViewName();
+
+                if ((myAccountNetworkTableFiltersAndSorting === null) ||
+                    ((myAccountNetworkTableFiltersAndSorting.lastReloadViewName) &&
+                    (myAccountNetworkTableFiltersAndSorting.lastReloadViewName === lastReloadViewName))) {
+
+                    myAccountNetworkTableFiltersAndSorting = {};
+                    myAccountNetworkTableFiltersAndSorting.filters = {};
+                    myAccountNetworkTableFiltersAndSorting.sorting = {};
+                    myAccountNetworkTableFiltersAndSorting.pageNumber = 1;
+
+                    if (myAccountNetworkTableFiltersAndSorting.lastReloadViewName) {
+                        delete myAccountNetworkTableFiltersAndSorting.lastReloadViewName;
+                    }
+                }
+
+                return myAccountNetworkTableFiltersAndSorting;
+            };
+
+            self.saveTableSettings = function(tableSettings) {
+                if (typeof(Storage) !== 'undefined') {
+                    var userName = self.getCurrentUserName();
+                    sessionStorage.setItem(userName, JSON.stringify(tableSettings));
+                }
+            };
+
+            self.initMyAccountNetworkTableFiltersAndSorting = function() {
+
+                var userName = self.getCurrentUserName();
+
+                var myAccountNetworkTableFiltersAndSorting =
+                    (typeof(Storage) !== 'undefined') ? JSON.parse(sessionStorage.getItem(userName)) : null;
+
+                var lastReloadViewName = self.getCurrentViewName();
+
+                if ((myAccountNetworkTableFiltersAndSorting === null) ||
+                    ((myAccountNetworkTableFiltersAndSorting.lastReloadViewName) &&
+                    (myAccountNetworkTableFiltersAndSorting.lastReloadViewName === lastReloadViewName))) {
+
+                    myAccountNetworkTableFiltersAndSorting = {};
+                    myAccountNetworkTableFiltersAndSorting.filters = {};
+                    myAccountNetworkTableFiltersAndSorting.sorting = {};
+                    myAccountNetworkTableFiltersAndSorting.pageNumber = 1;
+
+                    if (myAccountNetworkTableFiltersAndSorting.lastReloadViewName) {
+                        delete myAccountNetworkTableFiltersAndSorting.lastReloadViewName;
+                    }
+                }
+
+                return myAccountNetworkTableFiltersAndSorting;
+            };
+
+            self.clearLastReloadView = function(table) {
+                if (table.lastReloadViewName) {
+                    delete table.lastReloadViewName;
+                }
+            };
+
+            self.getCurrentViewName = function() {
+                var currentViewName = $location.path();
+
+                if (currentViewName) {
+
+                    var currentViewNameExtracted = currentViewName.split('/');
+
+                    if ((Array.isArray(currentViewNameExtracted) && (currentViewNameExtracted.length > 1))) {
+                        currentViewName = currentViewNameExtracted[1].toLowerCase();
+                    }
+                }
+                return currentViewName;
+            };
+
+
+            self.setLastReloadViewName = function () {
+
+                var currentViewName = self.getCurrentViewName();
+
+                var userName = self.getCurrentUserName();
+
+                if (typeof(Storage) !== 'undefined') {
+                    var myAccountNetworkTableFiltersAndSorting = JSON.parse(sessionStorage.getItem(userName));
+
+                    if (!myAccountNetworkTableFiltersAndSorting) {
+                        myAccountNetworkTableFiltersAndSorting = {};
+                        myAccountNetworkTableFiltersAndSorting.filters = {};
+                        myAccountNetworkTableFiltersAndSorting.sorting = {};
+                        myAccountNetworkTableFiltersAndSorting.pageNumber = 1;
+                    }
+                    myAccountNetworkTableFiltersAndSorting.lastReloadViewName = currentViewName;
+                    sessionStorage.setItem(userName, JSON.stringify(myAccountNetworkTableFiltersAndSorting));
+                }
+            };
+        }
+    ]);
